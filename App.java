dea770e29a1256def94cbe23649f0c7dac784be3
Gabriel <bielcheeeeee@gmail.com>
2024-03-07 22:20:19 -0300

    first commit

---
+++ App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro Esportivo = new Carro("esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        Carro Utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        Carro SUV = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
+        Carro SuvFlex = new Carro("SuvFlex", TipoCombustivel.FLEX, 0, 0);
+        // Carro Econo = new Carro("econo", TipoCombustivel.FLEX, 0, 55);
+
+
+    
+        System.out.println("Tipos de veiculos:");
+        System.out.println(basico);
+        System.out.println(SuvFlex);
+        System.out.println(SUV);
+        System.out.println(Utilitario);
+        System.out.println(Esportivo);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro basico com gasolina");
+        basico.abastece(TipoCombustivel.GASOLINA, 55);
+        System.out.println(basico);
+        System.out.println("\nViajando com o carro basico");
+        basico.viaja(250);
+        basico.viaja(150);
+        System.out.println(basico);
+    }
+}
---
+++ TanqueCombustivel.java
@@ -0,0 +1,58 @@
+public class TanqueCombustivel {
+
+    private TipoCombustivel tipoCombustivel;
+    private int capacidade;
+    private int combustivelDisponivel;
+
+    public TanqueCombustivel(TipoCombustivel tipoCombustivel, int capacidade) {
+        this.tipoCombustivel = tipoCombustivel;
+        this.capacidade = capacidade;
+        this.combustivelDisponivel = 0;
+    }
+
+    public TipoCombustivel getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public int getCombustivelDisponivel() {
+        return combustivelDisponivel;
+    }
+
+    public boolean abastece(TipoCombustivel tipoCombustivel, int quantidade) {
+        if (tipoCombustivel != this.tipoCombustivel) {
+            if (this.tipoCombustivel == TipoCombustivel.FLEX) {
+                if (!(tipoCombustivel == TipoCombustivel.GASOLINA || tipoCombustivel == TipoCombustivel.ALCOOL)) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        if (getCombustivelDisponivel() + quantidade > getCapacidade()) {
+            return false;
+        } else {
+            combustivelDisponivel += quantidade;
+            return true;
+        }
+    }
+
+    public boolean gasta(int quantidade) {
+        if (getCombustivelDisponivel() - quantidade < 0) {
+            return false;
+        } else {
+            combustivelDisponivel -= quantidade;
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "TanqueCombustivel [capacidade=" + capacidade + ", combustivelDisponivel=" + combustivelDisponivel
+                + ", tipoCombustivel=" + tipoCombustivel + "]";
+    }
+
+}
